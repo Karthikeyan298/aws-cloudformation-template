332eb0b42377ebb1a320aaacf471fb0604c776f3
Karthikeyan Vanaraj <kvanaraj@sevone.com>
2021-02-28 15:16:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import os
+import redis
+
+redis_host = os.environ.get("REDIS_HOST", "127.0.0.1")
+redis_port = os.environ.get("REDIS_port", "6379")
+try:
+    r = redis.Redis(host=redis_host, port=redis_port)
+except Exception as e:
+    print("Connection failed: {}".format(e))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/setdata/<key>/<value>')
+def setdata(key, value):
+    r.set(key, value)
+    return "success"
+
+@app.route('/getdata/<key>')
+def getdata(key):
+    return r.get(key)
+
+if __name__ == '__main__':
+    app.run()
